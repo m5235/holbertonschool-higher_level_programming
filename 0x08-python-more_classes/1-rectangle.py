0fda3f21588485aa018ed29047e09d78a8dc541a
m5235 <m.ladhari@outlook.com>
2020-05-25 23:33:57 +0100

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,11 +7,7 @@
 
 
 class Rectangle:
-    """
-    Rectangle:
-        methodes:
-            * __init__
-    """
+
     def __init__(self, width=0, height=0):
         """__init__: constructor"""
         self.width = width
@@ -19,12 +15,11 @@ class Rectangle:
 
     @property
     def width(self):
-        """width: retrieve width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width: set width"""
+ 
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -34,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """height: retrieve the height"""
+    
         return self.__height
 
     @height.setter
