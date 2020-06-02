ca20bd61486a6a78cfd0be82d35507b1bf9ed3ca
m5235 <m.ladhari@outlook.com>
2020-06-02 21:14:48 +0100

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,4 @@
 #!/usr/bin/pyhon3
-
 """
  function some classes
  """
@@ -9,7 +8,9 @@ def inherits_from(obj, a_class):
 
     """ function that returns True if the object
     is an instance of a class"""
-    if type(obj) is a_class:
+ if type(obj) is a_class:
         return False
+    if issubclass(type(obj), a_class):
+        return True
     else:
-        return issubclass(type(obj), a_class)
+        return False
