aa8c2dab250eba7c16d73d1981b52547837ae3fa
m5235 <m.ladhari@outlook.com>
2020-08-24 23:00:09 +0100

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     data = requests.get(str(sys.argv[1]))
-    if r.status_code != 200:
+    if data.status_code != 200:
         print("Error code: {}".format(data.status_code))
     else:
         print(data.text)
