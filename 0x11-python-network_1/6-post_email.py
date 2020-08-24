d5a744a741af66af89a161fd0f4805d3cf75b468
m5235 <m.ladhari@outlook.com>
2020-08-24 22:56:51 +0100

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,8 +2,9 @@
 """
 send post request using requests module.
 """
+import sys
 import requests
-from sys import argv
+
 if __name__ == "__main__":
     url = sys.argv[1]
     myobj = {"email": sys.argv[2]}
