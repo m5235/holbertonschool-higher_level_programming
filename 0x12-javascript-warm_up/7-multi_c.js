6d0fd88ab69d848974d23eff9ec4953f8b425f0f
m5235 <m.ladhari@outlook.com>
2020-10-16 22:57:42 +0100

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < (process.argv[2]); i++) {
-  console.log(Myarg);
+	  console.log(Myarg);
   }
 }
