31faf882eb3922dcd81fa4d61b26d9ba55ed1057
m5235 <m.ladhari@outlook.com>
2020-10-16 20:06:51 +0100

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const tab = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
-  console.log(tab[i]);
+  console.log(array[i]);
+}
