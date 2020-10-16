1fb42e6dd82f153b2749a87cde900a7127eab860
m5235 <m.ladhari@outlook.com>
2020-10-16 22:56:13 +0100

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const Myarg = 'C is fun';
 
 if (isNaN(process.argv[2])) {
-	console.log('Missing number of occurrences');
-  } else {
-	for (let i = 0; i < (process.argv[2]); i++) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < (process.argv[2]); i++) {
 	  console.log(Myarg);
-	}
   }
+}
