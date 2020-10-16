98343bf5c8fac654f37b8046575911f168487fbd
m5235 <m.ladhari@outlook.com>
2020-10-16 20:10:50 +0100

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 
-const aray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
-  console.log(aray[i]);
+  console.log(array[i]);
 }
