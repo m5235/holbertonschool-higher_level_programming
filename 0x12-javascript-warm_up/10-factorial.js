dbbb876493b9fda3d77daac80fe844a812312667
m5235 <m.ladhari@outlook.com>
2020-10-18 17:06:55 +0100

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 // computes and prints a factorial
 const n = process.argv[2];
 function factorial (number) {
-	if ((isNaN(number)) || (number === 1)) {
-	  return 1;
-	} else {
-		return parseInt(number) * factorial(parseInt(number) - 1);
-	}
+  if ((isNaN(number)) || (number === 1)) {
+    return 1;
+  } else {
+    return parseInt(number) * factorial(parseInt(number) - 1);
   }
-  
-  console.log(factorial(n));
+}
+
+console.log(factorial(n));
