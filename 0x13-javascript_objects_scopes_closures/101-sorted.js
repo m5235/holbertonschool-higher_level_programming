b7e560a279549bad66342057e3fa07f7a2d670d8
m5235 <m.ladhari@outlook.com>
2020-10-21 19:04:22 +0100

    done

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const dic = module.require('./101-data.js').dic;
+const Newdic = {};
+
+for (const k in dic) {
+  if (Newdic[dic[k]] !== undefined) {
+    Newdic[dic[k]].push(k);
+  } else {
+    Newdic[dic[k]] = [k];
+  }
+}
+console.log(Newdic);
