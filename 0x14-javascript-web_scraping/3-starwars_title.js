51309d6900cdec52e125273e8af855c011dc61e8
m5235 <m.ladhari@outlook.com>
2020-10-27 20:06:20 +0100

    task 1

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const Id = process.argv[2];
+const Url = `https://swapi-api.hbtn.io/api/films/` + Id;
+request(Url, (error, r, body) => {
+    if (error) { return console.log(error); }
+    console.log(body.title);
+});
