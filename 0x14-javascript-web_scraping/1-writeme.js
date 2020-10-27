9128b7fd59d93087ff1c8dddab4f797aec9a9787
m5235 <m.ladhari@outlook.com>
2020-10-27 19:48:00 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const { file } = require('babel-types');
-const f = require('compose-function');
 const fs = require('fs');
-const contentFile = process.argv[2];
-const toWrite = process.argv[3];
+const f = process.argv[2];
+const w = process.argv[3];
 
 fs.writeFile(f, w, 'utf-8', (error) => {
     if (error) throw error;
