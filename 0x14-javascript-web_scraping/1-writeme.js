f6af9931c92f24337445e741738c82c56bd75ef8
m5235 <m.ladhari@outlook.com>
2020-10-27 19:49:26 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const f = process.argv[2];
-const w = process.argv[3];
+const contentFile = process.argv[2];
+const toWrite = process.argv[3];
 
-fs.writeFile(f, w, 'utf-8', (error) => {
-    if (error) throw error;
+fs.writeFile(contentFile, toWrite, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
   }
 });
