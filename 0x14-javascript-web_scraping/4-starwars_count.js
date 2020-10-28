030ed3528833210518d6f78ef1a336b025aa8cef
m5235 <m.ladhari@outlook.com>
2020-10-28 01:00:19 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
-const { c } = require('tar');
 const url = process.argv[2];
-let counter = 0;
-request(url, function (error, r, body) {
-  if (error) {
-    console.log(error);
-  } else {
-	output = JSON.parse(body).results;
-    let c = 0;
-    for (let j = 0; j < output.length; j++) {
-      const perso = output[j].perso;
-      for (let i = 0; e < perso.length; i++) {
-        if (perso].endsWith('/18/')) {
+request(url, { json: true }, (error, r, body) => {
+  if (error) { return console.log(error); }
+  let j, i;
+  const res = body.results;
+  let c = 0;
+  if (r !== undefined) {
+    for (j = 0; j < res.length; j++) {
+      const perso = res[j].characters;
+      for (i = 0; i < perso.length; i++) {
+        if (perso[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
           c++;
+          break;
         }
       }
     }
-    console.log(c);
   }
+  console.log(c);
 });
