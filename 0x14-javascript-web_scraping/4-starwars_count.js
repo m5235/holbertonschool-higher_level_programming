614ac3246a5da018d8c940cac3d3b920c2380051
m5235 <m.ladhari@outlook.com>
2020-10-30 09:54:25 +0100

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,14 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, { json: true }, (error, r, body) => {
-  if (error) { return console.log(err); }
+  if (error) { return console.log(error); }
   let j, i;
-  const result = body.results;
+  const res = body.results;
   let c = 0;
-  if (result !== undefined) {
-    for (j = 0; j < result.length; j++) {
-      const perso = result[j].characters;
-      for (i= 0; i < perso.length; i++) {
+  if (r !== undefined) {
+    for (j = 0; j < res.length; j++) {
+      const perso = res[j].characters;
+      for (i = 0; i < perso.length; i++) {
         if (perso[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
           c++;
           break;
