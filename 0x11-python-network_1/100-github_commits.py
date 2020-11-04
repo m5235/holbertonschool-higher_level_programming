5491693568034043bffb47f4bd21ef2682b594e5
m5235 <m.ladhari@outlook.com>
2020-11-05 00:56:54 +0100

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
+
 """
 task 11
 """
-
 if __name__ == '__main__':
+
     import requests
-    import sys
+    from sys import argv
+
 
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
-
     commits = r.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
