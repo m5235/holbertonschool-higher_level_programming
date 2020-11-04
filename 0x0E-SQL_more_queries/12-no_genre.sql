1fccc35dc5e8173cd660acba4b754499e1879cf9
m5235 <m.ladhari@outlook.com>
2020-11-04 22:37:25 +0100

    done

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 -- in tv shows
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
-WHERE genre_id IS NULL
 RIGHT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
+WHERE genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
