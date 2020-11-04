0be0b81de29eae03f7d9fe095f7f36fa7dd7a190
m5235 <m.ladhari@outlook.com>
2020-11-04 23:12:04 +0100

    done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 
     import requests
     import sys
- url = 'https://api.github.com/repos/{}/{}/commits'
-    r = requests.get(url.format(sys.argv[2], sys.argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
     commits = r.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
