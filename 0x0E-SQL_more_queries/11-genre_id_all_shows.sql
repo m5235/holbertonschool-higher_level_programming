94c6cab841d7c003562b4139dfe349e7224ff763
m5235 <m.ladhari@outlook.com>
2020-11-04 22:23:53 +0100

    done

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- Genre ID by show
+-- select and join
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
