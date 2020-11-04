eb9d9cfa63ae32e2fc82ea0785ba897ddc88fd8b
m5235 <m.ladhari@outlook.com>
2020-11-04 22:56:46 +0100

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/user"
     auth = (argv[1], argv[2])
     response = requests.get(url, auth=auth)
-
     try:
         print(response.json().get("id"))
-
     except :
         print("Not a valid JSON")
