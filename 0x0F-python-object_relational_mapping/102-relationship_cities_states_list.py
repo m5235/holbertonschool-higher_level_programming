df054c95e273353a652cd1ed16bba10b143e6777
m5235 <m.ladhari@outlook.com>
2020-11-06 11:31:55 +0100

    task adv

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -2,21 +2,18 @@
 """
  task 102
 """
-from relationship_city import City
-from relationship_state import Base, State
-from sys import argv
-import sqlalchemy
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    state = session.query(State).order_by(State.id).all()
-    for stt in state:
-        for cit in stt.cities:
-            print("{}: {} -> {}".format(cit.id, cit.name, stt.name))
-    session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
