7534a8ca49b5429a38f3f73db704398f1ee19cb2
m5235 <m.ladhari@outlook.com>
2020-11-06 08:51:40 +0100

    task model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+task model
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State table
+    """
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, unique=True,
+                primary_key=True, auto_increment=True)
+    name = Column(String(128), nullable=False)
