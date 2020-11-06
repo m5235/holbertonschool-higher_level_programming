0dbc0cf4bd28f689c514567c9cc24d3775109c81
m5235 <m.ladhari@outlook.com>
2020-11-06 08:09:43 +0100

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-task 1
+task0
 """
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb as mysql
-    from sys import argv as args
 
-    # assign args to variables
-    mysql_username = args[1]
-    mysql_password = args[2]
-    db_name = args[3]
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor = db.cursor()
 
-    # connect to Mysql server
-    database = mysql.connect(
-        host="localhost",
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name)
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';\
-                    ORDER BY state.id")
+    rows = cursor.fetchall()
 
-    result = cursor.fetchall()
-    for i in result:
-        if i[1][0] == "N":
-            print(i)
+    for element in rows:
+        print(element)
+
+    cursor.close()
+    db.close()
