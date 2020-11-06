aefc79c05b561e1f90ac4d645dab6965e444a29e
m5235 <m.ladhari@outlook.com>
2020-11-06 10:55:22 +0100

    tas7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,12 +3,11 @@
 task7
 """
 
+import sys
 from model_state import Base, State
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, session
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
 if __name__ == "__main__":
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
